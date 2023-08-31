8d747bdfc3fd574669f6fbaa821fc513f6121979
Ian Torres <iantorres@outlook.com>
2023-08-31 03:25:37 -0400

    IndexController added.

---
+++ src/Http/Controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SpiritSaint\LaravelBacs\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class IndexController
+{
+    public function index(Request $request) {
+
+    }
+}
