c5d8e67fc3185750fe3bb2f1021b3c2fe0657280
Ian Torres <iantorres@outlook.com>
2023-08-31 17:14:14 -0400

    Controller implemented.

--- src/Http/Controllers/IndexController.php
+++ src/Http/Controllers/IndexController.php
@@ -3,11 +3,18 @@
 namespace SpiritSaint\LaravelBacs\Http\Controllers;
 
 
-use Illuminate\Http\Request;
+use SpiritSaint\LaravelBacs\Http\Requests\IndexRequest;
+use SpiritSaint\LaravelBacs\Records\HDR1;
+use SpiritSaint\LaravelBacs\Records\VOL;
 
 class IndexController
 {
-    public function index(Request $request) {
+    public function __invoke(IndexRequest $request) {
+        $response = [];
 
+        $response["data"]["vol"] = VOL::fromRequest($request);
+        $response["data"]["hdr1"] = HDR1::fromRequest($request);
+
+        return response()->json($response);
     }
 }
