09d2614a5487c35ecd543fd7c4888021622482a0
Ian Torres <iantorres@outlook.com>
2023-08-31 03:25:18 -0400

    Service provider added.

---
+++ src/Providers/LaravelBacsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SpiritSaint\LaravelBacs\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class LaravelBacsServiceProvider extends BaseServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+
+    }
+}
